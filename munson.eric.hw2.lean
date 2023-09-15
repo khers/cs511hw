4c1df810a5c197d39107679dd9bb90eccb1c4ade
Eric B Munson <munsoner@bu.edu>
2023-09-15 14:00:54 -0400

    Starting hw2

--- munson.eric.hw2.lean
+++ munson.eric.hw2.lean
@@ -3,3 +3,21 @@ import Library.Tactic.Extra
 import Library.Tactic.Numbers
 import Library.Tactic.Addarith
 import Library.Tactic.Cancel
+
+
+-- 6a
+example {x y : ℤ} (h1 : x + 3 ≥ 2 * y) (h2 : 1 ≤ y) : x ≥ -1 := by
+  calc
+    x ≥ 2 * y - 3 := by linarith
+    _ ≥ 2 * 1 - 3 := by rel [h2]
+    _ ≥ -1 := by numbers
+
+-- 6b
+example {a b : ℚ} (h1 : 3 ≤ a) (h2 : a + 2 * b ≥ 4) : a + b ≥ 3 := by
+  calc
+    
+
+-- 6c
+example {x : ℤ} (hx : x ≥ 9) : x ^ 3 - 8 * x ^ 2 + 2 * x ≥ 3 := by
+  calc
+  
