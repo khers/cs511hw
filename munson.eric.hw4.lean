2bc2c68d3b8604b4a3b5b753e43ca7a8d3c38c04
Eric B Munson <munsoner@bu.edu>
2023-09-28 13:52:27 -0400

    add place for hw4

---
+++ munson.eric.hw4.lean
@@ -0,0 +1,51 @@
+import Mathlib.Data.Real.Basic
+import Mathlib.Tactic.IntervalCases
+import Library.Theory.Comparison
+import Library.Theory.Parity
+import Library.Theory.Prime
+import Library.Tactic.ModCases
+import Library.Tactic.Extra
+import Library.Tactic.Numbers
+import Library.Tactic.Addarith
+import Library.Tactic.Cancel
+import Library.Tactic.Use
+
+-- 4a
+
+
+
+-- 4b
+
+
+
+-- 4c
+
+
+
+-- 4d
+
+
+
+-- 5a
+
+
+
+-- 5b
+
+
+
+-- 5c
+
+
+
+-- 5d
+
+
+
+-- 6a
+
+
+
+-- 6b
+
+
