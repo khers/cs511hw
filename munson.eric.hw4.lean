4cabc86a2f657d633e88e6c70a94d49b9ff51c0c
Eric B Munson <munsoner@bu.edu>
2023-10-03 12:13:48 -0400

    4b and c done too

--- munson.eric.hw4.lean
+++ munson.eric.hw4.lean
@@ -22,12 +22,24 @@ example {n : ℤ} (hn : Odd n) : Odd (7 * n - 4) := by
     _ = 2 * (7 * k + 1) + 1 := by ring
 
 -- 4b
-
-
+example {x y : ℤ} (hx : Odd x) (hy : Odd y) : Odd (x * y + 2 * y) := by
+  dsimp [Odd] at *
+  obtain ⟨a, ha⟩ := hx
+  obtain ⟨b, hb⟩ := hy
+  use 2 * a * b + a + 3 * b + 1
+  calc
+    x * y + 2 * y = (2 * a + 1) * (2 * b + 1) + 2 * (2 * b + 1) := by rw [ha, hb]
+    _ = 4 * a * b + 2 * a + 6 * b + 3 := by ring
+    _ = 2 * (2 * a * b + a + 3 * b + 1) + 1 := by ring
 
 -- 4c
-
-
+example {n : ℤ} (hn : Even n) : Odd (n ^ 2 + 2 * n - 5) := by
+  dsimp [Even, Odd] at *
+  obtain ⟨r, hr⟩ := hn
+  use 2 * r ^ 2 + 2 * r - 3
+  calc
+    n ^ 2 + 2 * n - 5 = (r + r)^2 + 2 * (r + r) - 5 := by rw [hr]
+    _ = 2 * (2 * r ^ 2 + 2 * r - 3) + 1 := by ring
 
 -- 4d
 
